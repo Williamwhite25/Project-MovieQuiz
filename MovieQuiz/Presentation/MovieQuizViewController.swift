3f2e2d7a9abb99bd89ae35b564f7cafcf6a1cb4e
William2025 <promo.for@yandex.ru>
2025-07-26 00:28:52 +0800

    сделаны все задачи по 7 спринту

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -3,11 +3,15 @@
 import UIKit
 import Foundation
 
+<<<<<<< HEAD
 
 final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate {
     private var statisticService: StatisticServiceProtocol!
     
     
+=======
+final class MovieQuizViewController: UIViewController, MovieQuizViewControllerProtocol  {
+>>>>>>> ff551c5 (сделаны все задачи по 7 спринту)
     @IBOutlet private weak var noButton: UIButton!
     @IBOutlet private weak var yesButton: UIButton!
     @IBOutlet private var imageView: UIImageView!
@@ -25,10 +29,14 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate {
    
     
     
+    private var alertPresenter: AlertPresenter!
+    private lazy var presenter = MovieQuizPresenter(viewController: self)
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+<<<<<<< HEAD
         textLable.font = UIFont(name: "YPDisplay-Bold", size: 23)
         counterLable.font = UIFont(name: "YPDisplay-Medium", size: 20)
         yesButton.titleLabel?.font = UIFont(name: "YPDisplay-Medium", size: 20)
@@ -114,6 +122,41 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate {
     }
     
     private func show(quiz step: QuizStepViewModel) {
+=======
+        presenter = MovieQuizPresenter(viewController: self)
+        alertPresenter = AlertPresenter()
+        
+        imageView.layer.cornerRadius = 20
+        noButton.layer.cornerRadius = 15
+        yesButton.layer.cornerRadius = 15
+        
+       
+        
+       
+    }
+    
+    
+    // MARK: - Actions
+    
+    @IBAction private func yesButtonClicked(_ sender: UIButton) {
+        presenter.yesButtonClicked()
+        
+        yesButton.isEnabled = false
+        noButton.isEnabled = false
+    }
+    
+    @IBAction private func noButtonClicked(_ sender: UIButton) {
+        presenter.noButtonClicked()
+        
+        yesButton.isEnabled = false
+        noButton.isEnabled = false
+    }
+    
+    // MARK: - Private functions
+    
+    func show(quiz step: QuizStepViewModel) {
+        imageView.layer.borderColor = UIColor.clear.cgColor
+>>>>>>> ff551c5 (сделаны все задачи по 7 спринту)
         imageView.image = step.image
         textLable.text = step.question
         counterLable.text = step.questionNumber
@@ -122,15 +165,41 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate {
         noButton.isEnabled = true
     }
     
+<<<<<<< HEAD
     
     private func showNextQuestionOrResults() {
         if currentQuestionIndex == questionsAmount - 1 {            displayQuizResults()
         } else {
             currentQuestionIndex += 1
             questionFactory.requestNextQuestion()
+=======
+    func show(quiz result: QuizResultsViewModel) {
+        let message = presenter.makeResultsMessage()
+        
+        let alert = UIAlertController(
+            title: result.title,
+            message: message,
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: result.buttonText, style: .default) { [weak self] _ in
+            guard let self = self else { return }
+            
+            self.presenter.restartGame()
+>>>>>>> ff551c5 (сделаны все задачи по 7 спринту)
         }
+        
+        alert.addAction(action)
+        
+        self.present(alert, animated: true, completion: nil)
     }
+
+    func highlightImageBorder(isCorrectAnswer: Bool) {
+            imageView.layer.masksToBounds = true
+            imageView.layer.borderWidth = 8
+            imageView.layer.borderColor = isCorrectAnswer ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
+        }
     
+<<<<<<< HEAD
     
     private func displayQuizResults() {
         statisticService.store(correct: correctAnswers, total: questionsAmount)
@@ -154,6 +223,33 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate {
             }))
         
         present(alert, animated: true)
+=======
+    func hideLoadingIndicator() {
+           activityIndicator.isHidden = true
+       }
+
+    func showLoadingIndicator() {
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+    }
+    
+    func showNetworkError(message: String) {
+        activityIndicator.isHidden = true
+
+        let alert = UIAlertController(
+            title: "Ошибка",
+            message: message,
+            preferredStyle: .alert)
+
+        let action = UIAlertAction(title: "Попробовать ещё раз",
+                style: .default) { [weak self] _ in
+            guard let self = self else { return }
+           
+            self.presenter.restartGame()
+        }
+
+        alert.addAction(action)
+>>>>>>> ff551c5 (сделаны все задачи по 7 спринту)
     }
 }
 
